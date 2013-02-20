15db8af65e752ff699f234954b0868c464beaa77
Ryan Ryan <ryan.anklam@gmail.com>
2013-02-20 07:26:36 -0600

    added build

---
+++ build.js
@@ -0,0 +1,12 @@
+({
+    name : 'main',
+    out : 'main-built.js',
+    baseUrl : 'js',
+    paths :  {
+        'appDir' : '../',
+        'baseUrl' : 'js',
+        'jquery' : 'vendor/jquery-1.9.1.min',
+        'backbone' : 'vendor/backbone',
+        'lodash' : 'vendor/lodash'
+    }
+})
